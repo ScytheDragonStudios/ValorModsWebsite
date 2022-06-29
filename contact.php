077403ef1b836a870b9b750e498470da034c080b
John Klaine Jr <56973447+ScytheDragonStudios@users.noreply.github.com>
2022-06-29 09:33:58 -0400

    subcommit2
    mini email update

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 if (isset($_POST['email']) && !empty($_POST['email']) {
     $subject = "New contact request: $_POST['subject']";
     $message = $_POST['body'];
-    $headers = 'From: info@website.com' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+    $headers = 'From: https://scythedragonstudios.github.io/ValorModsWebsite/contact.html' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 
     mail('kynpuppeteer@yahoo.com', $subject, $message, $headers);
 
